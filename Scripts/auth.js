ed5cc4e36290b37a58e9ce5a49f17d0110278487
Luiz Neto <luizneto.dev@outlook.com>
2025-07-15 18:54:05 -0300

    Ajuste

--- Scripts/auth.js
+++
@@ -1,7 +0,0 @@
-export function handleLogin(event) {
-  event.preventDefault();
-}
-
-export function handleRegister(event) {
-  event.preventDefault();
-}
--- Scripts/main.js
+++
@@ -1,6 +0,0 @@
-import { setupBlurEffect } from "./ui.js";
-import { handleLogin, handleRegister } from "./auth.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  setupBlurEffect();
-});
